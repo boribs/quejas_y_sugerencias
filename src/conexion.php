5de193725a7fbb0501a01900d799e2b4119231d0
boribs <christumer@gmail.com>
2023-05-21 18:20:37 -0600

    Actualización: Cambio de carpeta para .php
    Los .php irán en src, en vez de assets/conexiones

---
+++ src/conexion.php
@@ -0,0 +1,16 @@
+<?php
+    function Conectarse()
+    {
+        if (!($link=mysqli_connect("localhost","root","")))
+        {
+                echo "Error de conexion de la base de datos.";
+                exit();
+        }
+        if (!mysqli_select_db($link,"quejas_sugerencias_db"))
+        {
+                echo "Error seleccionando la base de datos.";
+                exit();
+        }
+        return $link;
+    }
+?>
