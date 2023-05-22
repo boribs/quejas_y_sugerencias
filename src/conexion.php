a7c910cff70d30eb5d1059f21f637ea3c0f3fff4
boribs <christumer@gmail.com>
2023-05-21 18:22:39 -0600

    Actualización: Formato *.php

--- src/conexion.php
+++ src/conexion.php
@@ -3,13 +3,13 @@
     {
         if (!($link=mysqli_connect("localhost","root","")))
         {
-                echo "Error de conexion de la base de datos.";
-                exit();
+            echo "Error de conexion de la base de datos.";
+            exit();
         }
         if (!mysqli_select_db($link,"quejas_sugerencias_db"))
         {
-                echo "Error seleccionando la base de datos.";
-                exit();
+            echo "Error seleccionando la base de datos.";
+            exit();
         }
         return $link;
     }
